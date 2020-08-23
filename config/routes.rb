c4d65af3335036a8c78e7fc098d21e73643a84a7
nakagawahiroki <iwbb1022@icloud.com>
2020-08-24 01:55:50 +0900

    homeページとhelpページに手を加えた

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
   get 'static_pages/home'
   get 'static_pages/help'
-=======
-  get 'staticpages/home'
-  get 'staticpages/help'
->>>>>>> a73da89a6294e842e5b28cd1a5d9a769c9945950
   root 'application#hello'
 end
