39e3bc579a766c7ed8780741420a3d362b6b4c1b
nakagawahiroki <iwbb1022@icloud.com>
2020-09-20 17:45:30 +0900

    13章完了

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,6 +6,7 @@ class MicropostsController < ApplicationController
   def create
 
     @micropost = current_user.microposts.build(micropost_params)
+    @micropost.image.attach(params[:micropost][:image])
 
     if @micropost.save
 
@@ -31,7 +32,7 @@ class MicropostsController < ApplicationController
 
   def micropost_params
 
-    params.require(:micropost).permit(:content)
+    params.require(:micropost).permit(:content,:image)
 
   end
 
